012b8ad6613ea17299da25f66983eec38f8114ab
Josué Cáceres <joskcres2@gmail.com>
2026-06-15 17:11:33 -0600

    Primera version funcional

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ class Producto {
     #enlace;
     #id;
     #cantidad;
-    constructor({ nombre, tipo, descripcion, precio, inventario, enlace }) {
+    constructor({ nombre, tipo, descripcion, precio, inventario, enlace, cantidad }) {
         this.nombre = nombre;
         this.#tipo = tipo;
         this.precio = precio
@@ -15,12 +15,14 @@ class Producto {
         this.#descripcion = descripcion;
         this.#id = Date.now() + Math.floor(Math.random() * 100)
         this.#enlace = enlace
+        this.#cantidad = cantidad
+    }
+    set cantidad(value) {
         this.#cantidad = 0
     }
     get cantidad() {
         return this.#cantidad
     }
-
     get id() {
         return this.#id
     }
@@ -70,12 +72,21 @@ class Producto {
         return this.#descripcion
     }
 
-    SumarProducto() {
-        this.#cantidad++
+    sumarProducto() {
+        return this.#cantidad += 1
+    }
+
+    restarProducto() {
+        return this.#cantidad -= 1
+    }
+
+    eliminarProducto() {
+        this.#cantidad = 0
     }
 
     obtenerTotal() {
-        return this.#precio * this.#cantidad
+        let total = this.#cantidad * this.#precio
+        return total
     }
 }
 
@@ -86,9 +97,9 @@ class Carrrito {
     #total;
     constructor(pedido) {
         this.#pedido = pedido
-        this.#impuesto = impuesto
-        this.#subTotal = subTotal
-        this.#total = total
+        this.#impuesto = this.tenerTotal() * 0.05
+        this.#total = this.tenerTotal()
+        this.#subTotal = this.obtenerSubtotal()
     }
     set pedido(value) {
         this.#pedido = value
@@ -97,105 +108,128 @@ class Carrrito {
         return this.#pedido
     }
 
-    set subTotal(value) {
-        let total = 0
-        this.#subTotal = this.pedido.map(producto => total += producto.precio)
-    }
-    get subTotal(){
+    get subTotal() {
         return this.#subTotal
     }
-    set impuesto(value) {
-        this.#impuesto = (this.subTotal * 12) / 100
-    }
 
-    get impuesto(){
+    get impuesto() {
         return this.#impuesto
     }
 
-    set total(value) {
-        this.#total =  this.subTotal - this.impuesto
+    get total() {
+        return this.#total
     }
 
-    get total(){
-        return this.#total
+    obtenerSubtotal() {
+        let casi = this.total - this.impuesto
+        return casi
+    }
+    tenerTotal() {
+        let sumar = 0
+        this.pedido.map(item => sumar += item.obtenerTotal())
+        return sumar
+    }
+
+    finalizarPedido() {
+        this.pedido.forEach(item => item.eliminarProducto())
     }
 
 }
 
+//puse los datos en objetos par que el orden de los parametros no afectara 
 let producto1 = new Producto({
     nombre: "Café Americano",
     precio: 12,
-    categoria: "Bebida caliente",
+    tipo: "Bebida caliente",
     descripcion: "Café negro tradicional",
     inventario: 20,
-    enlace: 'https://izzycooking.com/wp-content/uploads/2022/09/Americano-Coffee-thmbnail.jpg'
+    enlace: 'https://izzycooking.com/wp-content/uploads/2022/09/Americano-Coffee-thmbnail.jpg',
+    cantidad: 0
 })
 let producto2 = new Producto({
     nombre: "Café Latte",
     precio: 18,
-    categoria: "Bebida caliente",
+    tipo: "Bebida caliente",
     descripcion: "Café con leche espumada",
     inventario: 10,
-    enlace: 'https://ts4.mm.bing.net/th?id=OIP.gbP9OJ6qeWogd7poano4XwHaHa&pid=15.1&o=7&rm=3'
+    enlace: 'https://ts4.mm.bing.net/th?id=OIP.gbP9OJ6qeWogd7poano4XwHaHa&pid=15.1&o=7&rm=3',
+    cantidad: 0
 })
 let producto3 = new Producto({
     nombre: "Frappe de Chocolate",
     precio: 25,
-    categoria: "Bebida fría",
+    tipo: "Bebida fría",
     descripcion: "Bebida fría con chocolate y crema",
     inventario: 10,
-    enlace: 'https://dla-naturals.com/wp-content/uploads/2022/10/Dark-Chocolate-Frappe.png'
+    enlace: 'https://dla-naturals.com/wp-content/uploads/2022/10/Dark-Chocolate-Frappe.png',
+    cantidad: 0
 })
 let producto4 = new Producto({
     nombre: "Smoothie de Fresa",
     precio: 22,
-    categoria: "Bebida fría",
+    tipo: "Bebida fría",
     descripcion: "Batido natural de fresa",
     enlace: 'https://lasrecetasdemiabuela.recipesown.com/wp-content/uploads/2024/07/30-3-768x768.webp',
+    cantidad: 0,
     inventario: 5
 })
 let producto5 = new Producto({
     nombre: "Muffin de Vainilla",
     precio: 15,
-    categoria: "Postre",
+    tipo: "Postre",
     descripcion: "Pan dulce suave de vainilla",
     inventario: 5,
-    enlace: 'https://lasrecetasdemiabuela.recipesown.com/wp-content/uploads/2024/07/30-3-768x768.webp'
+    enlace: 'https://lepasteleria.com/wp-content/uploads/media/muffins-de-vainilla/muffins-de-vainilla-imagen-destacada.jpg',
+    cantidad: 0
 })
 let producto6 = new Producto({
     nombre: "Cheesecake",
     precio: 28,
-    categoria: "Postre",
+    tipo: "Postre",
     descripcion: "Pastel frío de queso",
     inventario: 3,
-    enlace: 'https://lepasteleria.com/wp-content/uploads/media/muffins-de-vainilla/muffins-de-vainilla-imagen-destacada.jpg'
+    enlace: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMAmSv-Jql6zg6aW8oN2VflqC_eEtdUj7DO0Yut-wSTA&s=10',
+    cantidad: 0
 })
 let producto7 = new Producto({
     nombre: "Sandwich de Pollo",
     precio: 30,
-    categoria: "Comida",
+    tipo: "Comida",
     descripcion: "Sandwich con pollo y vegetales",
     inventario: 3,
-    enlace: 'https://i.pinimg.com/originals/c8/5c/1c/c85c1caa0b4fa0735dd7462ae180256d.png'
+    enlace: 'https://i.pinimg.com/originals/c8/5c/1c/c85c1caa0b4fa0735dd7462ae180256d.png',
+    cantidad: 0
 })
 let producto8 = new Producto({
     nombre: "Bagel con Queso",
     precio: 20,
-    categoria: "Comida",
+    tipo: "Comida",
     descripcion: "Bagel tostado con queso crema",
     inventario: 4,
-    enlace: 'https://img.magnific.com/fotos-premium/bagel-queso-crema-desayuno_770123-7629.jpg'
+    enlace: 'https://img.magnific.com/fotos-premium/bagel-queso-crema-desayuno_770123-7629.jpg',
+    cantidad: 0
 })
 
 let contenedorProductos = document.querySelector('.productos')
 let contenedorPedido = document.querySelector('.pedido-container')
 let pedido = []
 let containerPedido = document.querySelector('.pedido-container')
+let containerTotal = document.querySelector('.total-container')
+let btnFinalizar = document.querySelector('.btn-finalizar')
+let productosFactura = document.querySelector('#listaProductos')
+let totalPagado = document.querySelector('#totalPagado')
+let carrito1 = []
+let reiniciar = document.querySelector('.reinicio')
+let input = document.querySelector('.buscador')
 let listaProductos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8]
-let accionesCarrito = document.querySelectorAll('.btn-group')
+let filtros = document.querySelector('.filtros')
+let nombre = ''
+let tipo = 'Todos'
+const dibujarProductos = (productos) => {
+    contenedorProductos.innerHTML = ''
+    for (let producto of productos) {
 
-for (let producto of listaProductos) {
-    let html = `<div class="col-md-3">
+        let html = `<div class="col-md-3">
                         <div class="card product-card">
                             <img src="${producto.enlace}" class="card-img-top" alt="${producto.nombre}">
                             <div class="card-body">
@@ -206,43 +240,121 @@ for (let producto of listaProductos) {
                             </div>
                         </div>
                     </div>`
-    contenedorProductos.innerHTML += html
+        contenedorProductos.innerHTML += html
+        let btnAgragar = document.querySelectorAll('.btn-agregar')
+        btnAgragar.forEach(item => {
+            item.addEventListener('click', (event) => {
+                let productoAgregar = productos.find(item => item.id == event.target.id)
+                if (!pedido.some(item => item.id == event.target.id)) {
+                    pedido.push(productoAgregar)
+                    productoAgregar.sumarProducto()
+                } else {
+                    productoAgregar.sumarProducto()
+                }
+                DibujarCarrito(pedido)
+            })
+        })
+    }
 }
 
 
-let btnsAgregar = document.querySelectorAll('.btn-agregar')
-btnsAgregar.forEach(btn => {
-    btn.addEventListener('click', (event) => {
-        let productoAgregar = listaProductos.find(item => item.id == event.target.id)
-        if (!pedido.some(item => item.id == event.target.id)) {
-            pedido.push(productoAgregar)
-            productoAgregar.SumarProducto()
-        } else {
-            productoAgregar.SumarProducto()
-        }
-        DibujarCarrito(pedido)
-        // productoAgregar.AgregarAlCArrito(productoAgregar)
-    })
-})
-const DibujarCarrito = (listaPedidos) => {
+const filtrar = (name, tipo) => {
+    let temporal = []
+    temporal = listaProductos.filter(item => (name != '') ? item.nombre.toLowerCase().includes(name.toLowerCase()) : item)
+        .filter(item => (tipo != 'Todos') ? item.tipo === tipo : item)
+    dibujarProductos(temporal)
+}
+
+const DibujarCarrito = (listaPedidos = []) => {
     containerPedido.innerHTML = ''
-    listaPedidos.forEach(producto => {
-        let html = `<div class="order-item d-flex justify-content-between align-items-center mb-3">
+    carrito1 = new Carrrito(listaPedidos.filter(item => item.cantidad > 0))
+    actualizarCarrito(carrito1)
+
+    carrito1.pedido.forEach(producto => {
+        if (producto.cantidad > 0) {
+            let html = `<div class="order-item d-flex justify-content-between align-items-center mb-3">
                     <div>
                         <strong>${producto.nombre}</strong><br>
                         Cantidad: ${producto.cantidad} | Q${producto.precio}.00 c/u
                     </div>
-                    <div class="btn-group" id="${producto.id}">
-                        <button class="btn btn-sm btn-control restar">-</button>
-                        <button class="btn btn-sm btn-control sumar">+</button>
-                        <button class="btn btn-sm btn-danger eliminar">Eliminar</button>
+                    <div class="btn-group id="botones">
+                        <button id="${producto.id}" class="btn btn-sm btn-control restar">-</button>
+                        <button id="${producto.id}" class="btn btn-sm btn-control sumar">+</button>
+                        <button id="${producto.id}" class="btn btn-sm btn-danger eliminar">Eliminar</button>
                     </div>
                     </div>`
-        containerPedido.innerHTML += html
+            containerPedido.innerHTML += html
+        }
+
+    }
+    )
+    let btnsAcciones = document.querySelectorAll('.btn-group')
+    btnsAcciones.forEach(btnsAccion => {
+        btnsAccion.addEventListener('click', (event) => {
+            let productoOperar = listaPedidos.filter(item => item.id == parseInt(event.target.id))
+            if (event.target.textContent == '+') {
+                productoOperar[0].sumarProducto()
+                productoOperar[0].obtenerTotal()
+            } else if (event.target.textContent == '-') {
+                productoOperar[0].restarProducto()
+                productoOperar[0].obtenerTotal()
+            } else {
+                productoOperar[0].eliminarProducto()
+                actualizarCarrito()
+            }
+            DibujarCarrito(pedido)
+
+        })
+
     })
+
 }
 
-let sumar = document.querySelectorAll('.sumar')
-sumar.forEach(btnSuma=>btnSuma.addEventListener('click',(event)=>{
-    console.log('hola')
-}))
+const actualizarCarrito = (carrito1) => {
+    containerTotal.innerHTML = ` <p>Subtotal: Q.${carrito1.subTotal.toFixed(2)}</p>
+                <p>Impuesto: 5%</p>
+                <h5>Total: Q.${carrito1.total.toFixed(2)}</h5>
+                </div>`
+    if (carrito1.subTotal == 0) {
+        btnFinalizar.classList.add('d-none')
+    } else {
+        btnFinalizar.classList.remove('d-none')
+    }
+}
+
+
+const MostrarPedido = (carrito1) => {
+    productosFactura.innerHTML = ''
+    let productosHTML = ''
+    carrito1.pedido.forEach(producto => {
+        productosHTML += `<li>${producto.nombre} x${producto.cantidad} = Q.${producto.obtenerTotal()}.00</li>`
+    })
+    productosFactura.innerHTML = productosHTML
+    totalPagado.innerHTML = `<p id="totalPagado"><strong>Total pagado: Q.${carrito1.total.toFixed(2)}</strong></p>`
+}
+
+filtros.addEventListener('click', (event) => {
+    tipo = event.target.value
+    if (tipo != undefined) {
+        filtrar(nombre, tipo)
+    } else {
+        tipo = 'Todos'
+        filtrar(nombre, tipo)
+    }
+})
+
+input.addEventListener('keyup', (event) => {
+    nombre = event.target.value
+    filtrar(nombre, tipo)
+})
+
+
+btnFinalizar.addEventListener('click', (event) => {
+    MostrarPedido(carrito1)
+    DibujarCarrito()
+    carrito1.finalizarPedido()
+
+})
+
+dibujarProductos(listaProductos)
+
